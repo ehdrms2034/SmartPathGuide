d8a8fd6071eb33db9c8c2c3c078b7878045ab27a
KimDongGeun <ehdrms2034@naver.com>
2020-05-14 14:59:03 +0900

    각 박물관에 따른 DB 학습 모델 생성 메소드 구현

--- backend/src/src/services/PathGuideService.ts
+++ backend/src/src/services/PathGuideService.ts
@@ -4,6 +4,7 @@ import { UserInfo } from "@models/UserInfo";
 import * as tf from "@tensorflow/tfjs-node";
 import { userInfo } from "os";
 import { layers, train, EarlyStopping } from "@tensorflow/tfjs-node";
+import path from 'path';
 
 class PathGuiderService {
   private userInfoDao: UserInfoDao;
@@ -20,8 +21,7 @@ class PathGuiderService {
     return paths;
   }
 
-
-  public async recommandAncient() {
+  public async makeAncientModel() {
 
     const trainSet = await this.getTrainSet();
     const testSet = await this.getTestSet();
@@ -50,7 +50,6 @@ class PathGuiderService {
       metrics: ["accuracy"],
     });
 
-    //model.summary();
     await model.fit(input, output, {
       epochs: 300,
       batchSize : 70,
@@ -64,8 +63,309 @@ class PathGuiderService {
     predict.print();
     // console.log(predict.shape);
     const test = model.predict(predict) as tf.Tensor;
-    console.log("이사람이 갈 확률 :",test.dataSync()[0]);
+    console.log("이사람이 antient 갈 확률 :",test.dataSync()[0]);
     
+    model.save("file://./src/TensorModel/ancient");
+
+    // console.log(acc[0]*100);
+    // model.predict(trainSet);
+  }
+
+  public async makeMedievalModel() {
+
+    const trainSet = await this.getTrainSet();
+    const testSet = await this.getTestSet();
+    const trainPaths = await this.getTrainPathSet();
+    const testPaths = await this.getTestPathSet();
+    const target = trainPaths.map((it) => it.medieval);
+    const testTarget = testPaths.map(it => it.medieval);
+
+    const input = tf.tensor(trainSet);
+    const output = tf.tensor(target);
+
+    const xTest = tf.tensor(testSet);
+    const yTest = tf.tensor(testTarget);
+
+    const model = tf.sequential({
+      layers: [
+        tf.layers.dense({ units: 64, inputShape: [7] }),
+        tf.layers.dense({ units: 8, activation: "relu" }),
+        tf.layers.dense({ units: 1, activation: "sigmoid" }),
+      ],
+    });
+
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: "binaryCrossentropy",
+      metrics: ["accuracy"],
+    });
+
+    await model.fit(input, output, {
+      epochs: 300,
+      batchSize : 70,
+      shuffle: true,
+      callbacks : [tf.callbacks.earlyStopping({monitor : 'acc'})]
+    });
+
+    // const scores = model.evaluate(xTest,yTest) as tf.Scalar[];
+    // const acc = scores[1].dataSync();
+    const predict = tf.tensor([[0,100,100,100,100,100,100]]);
+    predict.print();
+    // console.log(predict.shape);
+    const test = model.predict(predict) as tf.Tensor;
+    console.log("이사람이 medieval 갈 확률 :",test.dataSync()[0]);
+    
+    model.save("file://./src/TensorModel/medieval");
+
+    // console.log(acc[0]*100);
+    // model.predict(trainSet);
+  }
+
+  public async makeModernModel() {
+
+    const trainSet = await this.getTrainSet();
+    const testSet = await this.getTestSet();
+    const trainPaths = await this.getTrainPathSet();
+    const testPaths = await this.getTestPathSet();
+    const target = trainPaths.map((it) => it.modern);
+    const testTarget = testPaths.map(it => it.modern);
+
+    const input = tf.tensor(trainSet);
+    const output = tf.tensor(target);
+
+    const xTest = tf.tensor(testSet);
+    const yTest = tf.tensor(testTarget);
+
+    const model = tf.sequential({
+      layers: [
+        tf.layers.dense({ units: 64, inputShape: [7] }),
+        tf.layers.dense({ units: 8, activation: "relu" }),
+        tf.layers.dense({ units: 1, activation: "sigmoid" }),
+      ],
+    });
+
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: "binaryCrossentropy",
+      metrics: ["accuracy"],
+    });
+
+    await model.fit(input, output, {
+      epochs: 300,
+      batchSize : 70,
+      shuffle: true,
+      callbacks : [tf.callbacks.earlyStopping({monitor : 'acc'})]
+    });
+
+    // const scores = model.evaluate(xTest,yTest) as tf.Scalar[];
+    // const acc = scores[1].dataSync();
+    const predict = tf.tensor([[0,100,100,100,100,100,100]]);
+    predict.print();
+    // console.log(predict.shape);
+    const test = model.predict(predict) as tf.Tensor;
+    console.log("이사람이 modern 갈 확률 :",test.dataSync()[0]);
+    
+    model.save("file://./src/TensorModel/modern");
+
+    // console.log(acc[0]*100);
+    // model.predict(trainSet);
+  }
+
+  public async makePaintingModel() {
+
+    const trainSet = await this.getTrainSet();
+    const testSet = await this.getTestSet();
+    const trainPaths = await this.getTrainPathSet();
+    const testPaths = await this.getTestPathSet();
+    const target = trainPaths.map((it) => it.painting);
+    const testTarget = testPaths.map(it => it.painting);
+
+    const input = tf.tensor(trainSet);
+    const output = tf.tensor(target);
+
+    const xTest = tf.tensor(testSet);
+    const yTest = tf.tensor(testTarget);
+
+    const model = tf.sequential({
+      layers: [
+        tf.layers.dense({ units: 64, inputShape: [7] }),
+        tf.layers.dense({ units: 8, activation: "relu" }),
+        tf.layers.dense({ units: 1, activation: "sigmoid" }),
+      ],
+    });
+
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: "binaryCrossentropy",
+      metrics: ["accuracy"],
+    });
+
+    await model.fit(input, output, {
+      epochs: 300,
+      batchSize : 70,
+      shuffle: true,
+      callbacks : [tf.callbacks.earlyStopping({monitor : 'acc'})]
+    });
+
+    // const scores = model.evaluate(xTest,yTest) as tf.Scalar[];
+    // const acc = scores[1].dataSync();
+    const predict = tf.tensor([[0,100,100,100,100,100,100]]);
+    predict.print();
+    // console.log(predict.shape);
+    const test = model.predict(predict) as tf.Tensor;
+    console.log("이사람이 painting 갈 확률 :",test.dataSync()[0]);
+    
+    model.save("file://./src/TensorModel/painting");
+
+    // console.log(acc[0]*100);
+    // model.predict(trainSet);
+  }
+
+  public async makeWorldModel() {
+
+    const trainSet = await this.getTrainSet();
+    const testSet = await this.getTestSet();
+    const trainPaths = await this.getTrainPathSet();
+    const testPaths = await this.getTestPathSet();
+    const target = trainPaths.map((it) => it.world);
+    const testTarget = testPaths.map(it => it.world);
+
+    const input = tf.tensor(trainSet);
+    const output = tf.tensor(target);
+
+    const xTest = tf.tensor(testSet);
+    const yTest = tf.tensor(testTarget);
+
+    const model = tf.sequential({
+      layers: [
+        tf.layers.dense({ units: 64, inputShape: [7] }),
+        tf.layers.dense({ units: 8, activation: "relu" }),
+        tf.layers.dense({ units: 1, activation: "sigmoid" }),
+      ],
+    });
+
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: "binaryCrossentropy",
+      metrics: ["accuracy"],
+    });
+
+    await model.fit(input, output, {
+      epochs: 300,
+      batchSize : 70,
+      shuffle: true,
+      callbacks : [tf.callbacks.earlyStopping({monitor : 'acc'})]
+    });
+
+    // const scores = model.evaluate(xTest,yTest) as tf.Scalar[];
+    // const acc = scores[1].dataSync();
+    const predict = tf.tensor([[0,100,100,100,100,100,100]]);
+    predict.print();
+    // console.log(predict.shape);
+    const test = model.predict(predict) as tf.Tensor;
+    console.log("이사람이 world 갈 확률 :",test.dataSync()[0]);
+    
+    model.save("file://./src/TensorModel/world");
+
+    // console.log(acc[0]*100);
+    // model.predict(trainSet);
+  }
+
+  public async makeCraftModel() {
+
+    const trainSet = await this.getTrainSet();
+    const testSet = await this.getTestSet();
+    const trainPaths = await this.getTrainPathSet();
+    const testPaths = await this.getTestPathSet();
+    const target = trainPaths.map((it) => it.craft);
+    const testTarget = testPaths.map(it => it.craft);
+
+    const input = tf.tensor(trainSet);
+    const output = tf.tensor(target);
+
+    const xTest = tf.tensor(testSet);
+    const yTest = tf.tensor(testTarget);
+
+    const model = tf.sequential({
+      layers: [
+        tf.layers.dense({ units: 64, inputShape: [7] }),
+        tf.layers.dense({ units: 8, activation: "relu" }),
+        tf.layers.dense({ units: 1, activation: "sigmoid" }),
+      ],
+    });
+
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: "binaryCrossentropy",
+      metrics: ["accuracy"],
+    });
+
+    await model.fit(input, output, {
+      epochs: 300,
+      batchSize : 70,
+      shuffle: true,
+      callbacks : [tf.callbacks.earlyStopping({monitor : 'acc'})]
+    });
+
+    // const scores = model.evaluate(xTest,yTest) as tf.Scalar[];
+    // const acc = scores[1].dataSync();
+    const predict = tf.tensor([[0,100,100,100,100,100,100]]);
+    predict.print();
+    // console.log(predict.shape);
+    const test = model.predict(predict) as tf.Tensor;
+    console.log("이사람이 craft 갈 확률 :",test.dataSync()[0]);
+    
+    model.save("file://./src/TensorModel/craft");
+
+    // console.log(acc[0]*100);
+    // model.predict(trainSet);
+  }
+
+  public async makeDonationModel() {
+
+    const trainSet = await this.getTrainSet();
+    const testSet = await this.getTestSet();
+    const trainPaths = await this.getTrainPathSet();
+    const testPaths = await this.getTestPathSet();
+    const target = trainPaths.map((it) => it.donation);
+    const testTarget = testPaths.map(it => it.donation);
+
+    const input = tf.tensor(trainSet);
+    const output = tf.tensor(target);
+
+    const xTest = tf.tensor(testSet);
+    const yTest = tf.tensor(testTarget);
+
+    const model = tf.sequential({
+      layers: [
+        tf.layers.dense({ units: 64, inputShape: [7] }),
+        tf.layers.dense({ units: 8, activation: "relu" }),
+        tf.layers.dense({ units: 1, activation: "sigmoid" }),
+      ],
+    });
+
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: "binaryCrossentropy",
+      metrics: ["accuracy"],
+    });
+
+    await model.fit(input, output, {
+      epochs: 300,
+      batchSize : 70,
+      shuffle: true,
+      callbacks : [tf.callbacks.earlyStopping({monitor : 'acc'})]
+    });
+
+    // const scores = model.evaluate(xTest,yTest) as tf.Scalar[];
+    // const acc = scores[1].dataSync();
+    const predict = tf.tensor([[0,100,100,100,100,100,100]]);
+    predict.print();
+    // console.log(predict.shape);
+    const test = model.predict(predict) as tf.Tensor;
+    console.log("이사람이 donation 갈 확률 :",test.dataSync()[0]);
+    
+    model.save("file://./src/TensorModel/donation");
 
     // console.log(acc[0]*100);
     // model.predict(trainSet);
