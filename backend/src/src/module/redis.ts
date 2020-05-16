bedd0a6a9b7dfd4ffe2ff49525997ca007fd2292
KimDongGeun <ehdrms2034@naver.com>
2020-05-16 16:41:55 +0900

    redis expire 설정

--- backend/src/src/module/redis.ts
+++ backend/src/src/module/redis.ts
@@ -3,8 +3,13 @@ import { resolve, reject } from 'bluebird';
 
 const redisClient = new redis.RedisClient({host : "localhost",port:6379});
 
-export const redisSetData = (key: string,value : any) => {
-    redisClient.set(key,value);
+export const redisSetData = async (key: string,value : any) => {
+    return new Promise((resolve,reject)=>{
+        redisClient.setex(key,60*2,value,(err,reply)=>{
+            if(err) reject(err);
+            return resolve(reply);
+        });
+    })
 }
 
 export const redisGetData = (key : string) => {
