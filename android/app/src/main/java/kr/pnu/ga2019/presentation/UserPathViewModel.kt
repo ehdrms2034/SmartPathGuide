306f9048f0bfb3aa34a9ef373671d8eaa661af3b
LeeOhHyung <lee2928030@naver.com>
2020-05-28 02:13:02 +0900

    [PR-6] AppSchedulerProvider, SingleLiveEvent, ViewModel 생성자에서 필요한 클래스들 생성

--- android/app/src/main/java/kr/pnu/ga2019/presentation/UserPathViewModel.kt
+++ android/app/src/main/java/kr/pnu/ga2019/presentation/UserPathViewModel.kt
@@ -5,8 +5,29 @@ package kr.pnu.ga2019.presentation
 
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
+import kr.pnu.ga2019.data.repository.PathRepositoryImpl
+import kr.pnu.ga2019.data.repository.RecommendRepositoryImpl
+import kr.pnu.ga2019.data.repository.UserInfoRepositoryImpl
+import kr.pnu.ga2019.data.repository.UserRepositoryImpl
+import kr.pnu.ga2019.domain.repository.PathRepository
+import kr.pnu.ga2019.domain.repository.RecommendRepository
+import kr.pnu.ga2019.domain.repository.UserInfoRepository
+import kr.pnu.ga2019.domain.repository.UserRepository
+import kr.pnu.ga2019.util.AppSchedulerProvider
+import kr.pnu.ga2019.util.BaseSchedulerProvider
 
-class UserPathViewModel : ViewModel() {
+class UserPathViewModel(
+    private val userRepository: UserRepository =
+        UserRepositoryImpl(),
+    private val userInfoRepository: UserInfoRepository =
+        UserInfoRepositoryImpl(),
+    private val pathRepository: PathRepository =
+        PathRepositoryImpl(),
+    private val recommendRepository: RecommendRepository =
+        RecommendRepositoryImpl(),
+    private val scheduler: BaseSchedulerProvider =
+        AppSchedulerProvider()
+) : ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
 
