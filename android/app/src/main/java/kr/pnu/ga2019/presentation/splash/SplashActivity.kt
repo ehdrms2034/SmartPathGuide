c924aa9a743076c27df3548e64dce02a47a70076
LeeOhHyung <lee2928030@naver.com>
2020-06-03 18:06:38 +0900

    새로운 Splash 화면 제작

---
+++ android/app/src/main/java/kr/pnu/ga2019/presentation/splash/SplashActivity.kt
@@ -0,0 +1,21 @@
+/*
+ * Created by Lee Oh Hyoung on 2020/06/03 .. 
+ */
+package kr.pnu.ga2019.presentation.splash
+
+import androidx.lifecycle.ViewModelProvider
+import kr.pnu.ga2019.R
+import kr.pnu.ga2019.databinding.ActivitySplashBinding
+import kr.pnu.ga2019.presentation.base.BaseActivity
+
+class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>(
+    resourceId = R.layout.activity_splash
+) {
+    override val viewModel: SplashViewModel by lazy {
+        ViewModelProvider(this).get(SplashViewModel::class.java)
+    }
+
+    override fun start() {
+        /* explicitly empty */
+    }
+}
---
+++ android/app/src/main/java/kr/pnu/ga2019/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,10 @@
+/*
+ * Created by Lee Oh Hyoung on 2020/06/03 .. 
+ */
+package kr.pnu.ga2019.presentation.splash
+
+import kr.pnu.ga2019.presentation.base.BaseViewModel
+
+class SplashViewModel : BaseViewModel() {
+
+}
