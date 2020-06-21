d5977ac9ea501e6453bad0e21ef1144b156eed1e
KimDongGeun <ehdrms2034@naver.com>
2020-06-21 14:21:59 +0900

    [Feature-add]svd + lstm + swagger

--- backend/src/src/routes/Paths.ts
+++ backend/src/src/routes/Paths.ts
@@ -1,10 +1,9 @@
 import { Router, Request, Response } from "express";
-import { PathsDao, RequestPath } from "@daos/pathsDao";
+import { PathsDao } from "@daos/pathsDao";
 import { Member } from "src/model/member";
 import { ApiResponse } from "@models/response";
 import { CustomError } from "@models/CustomError";
 import MemberDao from "@daos/memberDao";
-import { INSUFFICIENT_SPACE_ON_RESOURCE } from "http-status-codes";
 
 const router = Router();
 const pathsDao = new PathsDao();
@@ -12,38 +11,11 @@ const pathsDao = new PathsDao();
 router.post("/", async (req: Request, res: Response) => {
   const {
     memberSeq,
-    isAncient,
-    isMedieval,
-    isModern,
-    isDonation,
-    isPainting,
-    isWorld,
-    isCraft,
-    isScience,
-    isSpace,
-    isHuman,
-    isNatural,
-    isFuture,
   } = req.body;
   const memberDao = new MemberDao();
 
   try {
     const member: Member = await memberDao.getMemberBySeq(memberSeq);
-    const requestPath: RequestPath = {
-      ancient: isAncient,
-      medieval: isMedieval,
-      modern: isModern,
-      donation: isDonation,
-      painting: isPainting,
-      world: isWorld,
-      craft: isCraft,
-      science: isScience,
-      space : isSpace,
-      human : isHuman,
-      natural: isNatural,
-      future: isFuture
-    };
-    const newPaths = await pathsDao.createPaths(member, requestPath);
 
     res.json(
       new ApiResponse(
--- backend/src/src/routes/Users.ts
+++ backend/src/src/routes/Users.ts
@@ -1,57 +1,57 @@
-import { Request, Response, Router } from "express";
-import { ApiResponse } from "@models/response";
+// import { Request, Response, Router } from "express";
+// import { ApiResponse } from "@models/response";
 
-import MemberDao from "@daos/memberDao";
-import UserInfoDao from "@daos/UserInfoDao";
-import { UserInfo } from "@models/UserInfo";
+// import MemberDao from "@daos/memberDao";
+// import UserInfoDao from "@daos/UserInfoDao";
+// import { UserInfo } from "@models/UserInfo";
 
-// Init shared
-const router = Router();
-const memberDao = new MemberDao();
-const userInfoDao = new UserInfoDao();
+// // Init shared
+// const router = Router();
+// const memberDao = new MemberDao();
+// const userInfoDao = new UserInfoDao();
 
-router.get("/", async (req: Request, res: Response) => {});
+// router.get("/", async (req: Request, res: Response) => {});
 
-router.get("/members", async (req: Request, res: Response) => {
-  try{
-    const data = await memberDao.getAllMembers();
-    return res.json(new ApiResponse("success","리스트 출력에 성공했습니다.",data));
-  }catch(e){
-    res.json(new ApiResponse("error", e, null));
-  }
-});
+// router.get("/members", async (req: Request, res: Response) => {
+//   try{
+//     const data = await memberDao.getAllMembers();
+//     return res.json(new ApiResponse("success","리스트 출력에 성공했습니다.",data));
+//   }catch(e){
+//     res.json(new ApiResponse("error", e, null));
+//   }
+// });
 
-router.post("/", async (req: Request, res: Response) => {
-  const {
-    age,
-    ancient,
-    medieval,
-    modern,
-    donation,
-    painting,
-    world,
-    craft,
-  } = req.body;
-  try {
-    const newUser = await memberDao.saveMember(age);
-    const requestInfo: UserInfo = new UserInfo({
-      ancient,
-      medieval,
-      modern,
-      donation,
-      painting,
-      world,
-      craft,
-    });
-    await userInfoDao.createUserInfo(newUser.userSeq);
-    await userInfoDao.setUserInfo(newUser.userSeq, requestInfo);
-    const response = new ApiResponse("success", "회원추가에 성공했습니다.", {
-      memberPK: newUser.userSeq,
-    });
-    return res.json(response);
-  } catch (error) {
-    res.json(new ApiResponse("error", error, null));
-  }
-});
+// router.post("/", async (req: Request, res: Response) => {
+//   const {
+//     age,
+//     ancient,
+//     medieval,
+//     modern,
+//     donation,
+//     painting,
+//     world,
+//     craft,
+//   } = req.body;
+//   try {
+//     const newUser = await memberDao.saveMember(age);
+//     const requestInfo: UserInfo = new UserInfo({
+//       ancient,
+//       medieval,
+//       modern,
+//       donation,
+//       painting,
+//       world,
+//       craft,
+//     });
+//     await userInfoDao.createUserInfo(newUser.userSeq);
+//     await userInfoDao.setUserInfo(newUser.userSeq, requestInfo);
+//     const response = new ApiResponse("success", "회원추가에 성공했습니다.", {
+//       memberPK: newUser.userSeq,
+//     });
+//     return res.json(response);
+//   } catch (error) {
+//     res.json(new ApiResponse("error", error, null));
+//   }
+// });
 
-export default router;
+// export default router;
